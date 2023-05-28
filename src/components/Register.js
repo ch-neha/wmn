ade280c42f9b893c9af150ee07400619af4752cd
Neha <23nehachekuri@gmail.com>
2023-05-28 08:44:43 +0530

    register

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,185 @@
+import React, { useState, useEffect, useRef } from 'react';
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import {
+  TextField,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+} from "@material-ui/core";
+
 export default function Register() {
+
+    const [orgDesc,setOrgDesc] = useState('');
+    const [orgName, setOrgName] = useState('');
+    const [orgLocation, setOrgLocation] = useState('');
+    const [profilePhoto, setProfilePhoto] = useState(null);
+    const [isSubmitted, setIsSubmitted] = useState(false);
+    const [eventObj, setEventObj] = useState(['']);
+    const [email, setEmail] = useState('');
+    const [phoneNo, setPhoneNo] = useState('');
+   
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        //use the values for apis
+        // console.log(eventTitle);
+        // console.log(orgName);
+        // console.log(eventStartDate);
+        // console.log(eventEndDate);
+        // console.log(eventLocation);
+        // console.log(eventDescription);
+        // console.log(eventLink);
+        // console.log(eventPoster);
+
+        // Reset the form fields after submission
+
+        const object = [orgName, orgDesc , orgLocation, phoneNo, email, profilePhoto];
+        console.log(object);
+
+        setEventObj(object);
+        console.log(eventObj);
+
+        // setOrgDesc('');
+        // setOrgName('');
+        // setOrgLocation('');
+        // setEmail('');
+        // setPhoneNo('');
+        // setProfilePhoto(null);
+        // setIsSubmitted(false);
+        // setEventObj(null);
+
+        // document.getElementById("regform").reset();
+
+        //alert message to display successful submission
+        setIsSubmitted(true);
+        setTimeout(()=>{
+            setIsSubmitted(false)
+        }, 2000);
+
+        console.log(eventObj);
+
+        console.log("setIsSubmitted");
+    }
+
+    useEffect(()=>{
+        
+
+    },eventObj)
+
     return (
         <div>
-            
+            <Container sx={{ width: "200" }}>
+      <Box
+        sx={{
+        width: "75%", 
+        display: "flex",
+        flexDirection: "column",
+        boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.2)",
+        margin: "1rem",
+        marginLeft: "10rem",
+        backgroundColor: "#ffffff",
+        padding: "2rem",
+        paddingRight: "16rem"
+        }}
+        
+      >
+        <Typography component="h1" variant="h5">
+          Profile Details
+        </Typography>
+        <Box component="form" id='regform' noValidate onSubmit={handleSubmit}>
+        
+          <br/>
+          <TextField
+            label="Organization Name"
+            type="text"
+            value={orgName}
+            onChange={(e) => setOrgName(e.target.value)}
+            InputLabelProps={{
+                shrink: true
+            }}
+            margin="normal"
+            fullWidth
+            required
+          />
+          <br/><br/>
+          <TextField
+            label="Organisation Description"
+            type="text"
+            value={orgDesc}
+            onChange={(e) => setOrgDesc(e.target.value)}
+            fullWidth
+            InputLabelProps={{
+                shrink: true
+            }}
+            required
+          />
+          <TextField
+            label="Address"
+            type="text"
+            value={orgLocation}
+            onChange={(e) => setOrgLocation(e.target.value)}
+            InputLabelProps={{
+                shrink: true
+            }}
+            margin="normal"
+            fullWidth
+            required
+          />
+          <br/><br/>
+          <TextField
+            label="Email ID:"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            fullWidth
+            InputLabelProps={{
+                shrink: true
+            }}
+            required
+          />
+          <br/><br/>
+          
+          <TextField
+            label="Contact Number:"
+            type="number"
+            value={phoneNo}
+            onChange={(e) => setPhoneNo(e.target.value)}
+            fullWidth
+            InputLabelProps={{
+                shrink: true
+            }}
+            required
+          />
+          <br/><br/>
+          <FormControl margin="normal" fullWidth>
+           <FormLabel required>Upload Profile Photo</FormLabel>
+            <Input 
+               type="file" 
+               required 
+               value={profilePhoto}
+               onChange={(e) => setProfilePhoto(e.target.value)}
+               />
+          </FormControl>
+        
+          <div className='mt-2'>
+            {isSubmitted && (
+                <div className="alert alert-success mt-3" role="alert">
+                Event submitted successfully!
+                </div>
+            )}
+        </div>
+          <Button type="submit" variant="contained" color="primary" className='mt-3'
+            disabled={!orgDesc || !orgName || !phoneNo || !email || !orgLocation || !profilePhoto }
+            >
+            Submit
+          </Button>
+    
+        </Box>
+      </Box>
+    </Container>
         </div>
     );
 }
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,38 +1,67 @@
-import React from "react";
-import WhatsAppIcon from '@mui/icons-material/WhatsApp';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import IconButton from "@mui/material/IconButton";
-import FacebookRoundedIcon from '@mui/icons-material/FacebookRounded';
+import React, { useState } from "react";
 
 function Users() {
+
+    const [users, setusers] = useState([
+        {
+            'imgurl': 'https://turinghut.org/static/9384c958bbd95db9f010ee70a3bdaee1/30cdc/turinghut_logo.png',
+            'name':'Turing Hut',
+            'desc':'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam exobcaecati molestiaesequi magni odit sapiente autem ea doloremque natusaliquid dolor cupiditate inventore corrupti nam?',
+            'phone':'+919872362435',
+            'email':'turinghut@gmail.com'
+        },
+        {
+            'imgurl':'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Association_for_Computing_Machinery_%28ACM%29_logo.svg/1200px-Association_for_Computing_Machinery_%28ACM%29_logo.svg.png',
+            'name':'CSI',
+            'desc':'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam exobcaecati molestiaesequi magni odit sapiente autem ea doloremque natusaliquid dolor cupiditate inventore corrupti nam?',
+            'phone':'+919872362435',
+            'email':'csiindia@gmail.com'
+        },
+        {
+            'imgurl':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj92UzpcNh8gvuPWNapxP6wdjXQ5Xm4NuDVI22E7s&s',
+            'name':'EDCell',
+            'desc':'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam exobcaecati molestiaesequi magni odit sapiente autem ea doloremque natusaliquid dolor cupiditate inventore corrupti nam?',
+            'phone':'+919872362435',
+            'email':'ecell@gmail.com'
+        },
+        {
+            'imgurl':'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Association_for_Computing_Machinery_%28ACM%29_logo.svg/1200px-Association_for_Computing_Machinery_%28ACM%29_logo.svg.pngp',
+            'name':'ACM',
+            'desc':'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam exobcaecati molestiaesequi magni odit sapiente autem ea doloremque natusaliquid dolor cupiditate inventore corrupti nam?',
+            'phone':'+919872362435',
+            'email':'acm@gmail.com'
+        },
+    ]);
+
     return (
         <div className="container">
-            
-            <div className="card mt-5 mb-3 w-4 shadow">
+
+            {
+
+                users.map((ele) => <div className="card mt-5 mb-3 shadow">
                 <div className="card-body">
                     <div className="row">
                         <div className="col-3">
-                            <img src="https://mdbcdn.b-cdn.net/img/new/avatars/1.webp"
+                            <img src={ele.imgurl}
                                 className="card-img rounded-circle shadow-4-strong" width="40px" alt=""></img>
                         </div>
                         <div className="col-9">
-                            <div className="display-6">Alan</div>
-                            <p className="card-text pt-2">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam,
-                                ex
-                                obcaecati molestiae
-                                sequi magni odit sapiente autem ea doloremque natus
-                                aliquid dolor cupiditate inventore corrupti nam?</p>
+                            <div className="display-6">{ele.name}</div>
+                            <p className="card-text pt-2">{ele.desc}</p>
                             <br></br>
-                            <p>Contact No: +9112345688</p>
-                            <p>Email Address: test@gmail.com</p>
+                            <p>Contact No: {ele.phone}</p>
+                            <p>Email Address: {ele.email}</p>
                             <div className="row">
-                            <div><i className="fa fa-spinner fa-spin">no spinner but why</i></div>
+                                
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </div>)
+
+            }
+            
+            
             
         </div>
     )
